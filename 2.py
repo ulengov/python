5d6ccbc7fec05e0eeac98e85c7ed3f68b6fba20c
Veniamin Ulengov <76042388+ulengov@users.noreply.github.com>
2020-12-24 00:53:04 +1000

    Update 2.py

--- 2.py
+++ 2.py
@@ -2,7 +2,7 @@ arg = int(input('Введите время в секундах: '))
 
 hour = arg // 3600
 min = (arg - hour * 3600) // 60
-sec = arg - 3600 * hour - 60 * min
+sec = arg % 60
 
 print(f'{hour:02}:{min:02}:{sec:02}')
 
