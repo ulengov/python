954c82d2309ff6ec91e2fa63de917edcddd269e6
ulengov <ulengov@mail.ru>
2021-01-15 13:40:00 +1000

    patch

--- 4.py
+++ 4.py
@@ -1,7 +1,7 @@
 num =  [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
-dict = {i:num.count(i) for i in num}
+#dict = {i:num.count(i) for i in num}
 
-res = [el for el in num if dict[el] == 1]
+res = [el for el in num if num.count(el) == 1]
 
 print(res)
